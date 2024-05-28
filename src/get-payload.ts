a802a74793c9a79beecb700cd39e213d3a6dc43b
sushanth <sushanthm1611@gmail.com>
2024-05-28 23:03:28 +0530

    inital commit

---
+++ src/get-payload.ts
@@ -0,0 +1,65 @@
+import dotenv from "dotenv";
+import path from "path";
+import { InitOptions } from "payload/config";
+import payload, { Payload } from "payload";
+import nodemailer from "nodemailer";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
+var transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASS,
+  },
+});
+
+let cached = (global as any).payload;
+
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  };
+}
+
+interface Args {
+  initOptions?: Partial<InitOptions>;
+}
+
+export const getPayloadClient = async ({
+  initOptions,
+}: Args = {}): Promise<Payload> => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error("PAYLOAD_SECRET is missing");
+  }
+
+  if (cached.client) {
+    return cached.client;
+  }
+
+  if (!cached.promise) {
+    cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "sushanthm1611@gmail.com",
+        fromName: "DigitalHippo",
+      },
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+
+  try {
+    cached.client = await cached.promise;
+  } catch (e: unknown) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.client;
+};
